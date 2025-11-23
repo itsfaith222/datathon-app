ebacba9d93db7523d881a53bd9789a59ac87ca6a
Faith <Faithaikhionbare@outlook.com>
2025-11-23 02:20:10 -0500

    Created a new branch so i can try to create the part where user inputs restrictions withou messing up our functional main

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__, static_folder=os.path.join(BASE_DIR, "..", "frontend"), static_url_path="/")
 CORS(app)
 
-
 @app.route("/")
 def index():
     # Serve frontend index.html
